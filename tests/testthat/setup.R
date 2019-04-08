0be9bf9ddda1dbee55c401b9ec0743af742dd9eb
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-04-08 14:13:47 -0400

    try suggesting osmar for tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,10 +1,14 @@
 suppressMessages({
+  if (!requireNamespace("osmar", quietly = TRUE)) stop("Must have osmar installed to run tests")
   boston_xml_file <- unzip(system.file("extdata/boston.xml.zip", package = "bigosm", mustWork = TRUE), exdir = tempdir())
 
   way_keys <- "highway"
   relation_keys <- "type"
 
+  # Object to test the key filtering functioons
   boston_osmar <- read_big_osm(boston_xml_file, way_keys = way_keys, relation_keys = relation_keys)
-  reference_osmar <- get_osm(complete_file(), source = osmsource_file(boston_xml_file))
+
+  # Get full object and osmar reference object
+  reference_osmar <- osmar::get_osm(osmar::complete_file(), source = osmar::osmsource_file(boston_xml_file))
   complete_big_osmar <- read_big_osm(boston_xml_file)
 })
